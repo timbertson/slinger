550398b17d685362bbbf94b00a697a50b79a9ed7
Tim Cuthbertson <tim@gfxmonk.net>
2019-08-07 21:42:42 +1000

    refactor SplitMode to live on menu selection, adjust six-split UI

--- src/menu_handlers.ts
+++ src/menu_handlers.ts
@@ -17,14 +17,25 @@ module MenuHandlers {
 	const ANGLE_SIXTEENTH = ANGLE_EIGHTH / 2;
 	const ANGLE_TWELFTH = TAO / 12.0;
 
-	function circularIndex(sections: number, offset: number) {
+	function circularIndex(sections: number, offset: number, mapFn: (_: number) => number) {
 		const span = TAO / sections;
 		return function(angle: number) {
-			return floor((angle + PI + offset) / span) % sections;
+			return mapFn(floor((angle + PI + offset) / span) % sections);
 		}
 	}
 
-	const outerIndex = circularIndex(8, ANGLE_SIXTEENTH);
+	function selectionOfIndex(selections: number[]) {
+		return function(index: number) {
+			return selections[index];
+		}
+	}
+
+	const identity = function(x: any) { return x; };
+	const outerSelection = circularIndex(8, ANGLE_SIXTEENTH, identity);
+	const innerSelectionFour = circularIndex(2, 0,
+		selectionOfIndex([Anchor.TOP, Anchor.BOTTOM]));
+	const innerSelectionSix = circularIndex(3, ANGLE_TWELFTH,
+		selectionOfIndex([Anchor.TOPLEFT, Anchor.TOPRIGHT, Anchor.BOTTOM]));
 
 	function floatColor(c: Color): Color {
 		return {
@@ -45,10 +56,8 @@ module MenuHandlers {
 		private preview: Preview.LayoutPreview<any>;
 		private canvas: ClutterCanvas;
 		private Sys: System<any>;
-		private menu: Menu.Menu<any>;
 
-		constructor(menu: Menu.Menu<any>, Sys: System<any>, menuSize: Point, origin: Point, canvas: ClutterCanvas, preview: Preview.LayoutPreview<any>) {
-			this.menu = menu;
+		constructor(Sys: System<any>, splitMode: SplitMode, menuSize: Point, origin: Point, canvas: ClutterCanvas, preview: Preview.LayoutPreview<any>) {
 			this.Sys = Sys;
 			this.currentMouseRelative = Point.ZERO;
 			this.origin = origin;
@@ -64,17 +73,16 @@ module MenuHandlers {
 			const HALF_GAP_WIDTH = floor(GAP_WIDTH / 2);
 			const EDGE_WIDTH = floor(OUTER_RADIUS * 0.34);
 			const CORNER_WIDTH = floor(OUTER_RADIUS * 0.4);
-			const CORNER_DISTANCE = floor(OUTER_RADIUS * 0.8);
+			const CORNER_DISTANCE = floor(OUTER_RADIUS * 0.78);
+			const EDGE_ARC_DISTANCE = floor(OUTER_RADIUS * 1.1);
 			const GLYPH_WIDTH = floor(OUTER_RADIUS * 0.09);
 			const GLYPH_THICKNESS = floor(OUTER_RADIUS * 0.03);
-			const DARK_FOUR = floatColor({ r: 0, g: 77, b: 101, a: 200 });
-			const DARK_SIX = floatColor({ r: 128, g: 77, b: 0, a: 200 });
+			const DARK = floatColor({ r: 18, g: 36, b: 48, a: 200 });
 			const LIGHT = floatColor({ r: 66, g: 79, b: 92, a: 237 });
 			const BG = { luminance: 0.7, alpha: 0.7 };
-			const ACTIVE_FOUR = floatColor({ r: 0, g: 155, b: 203, a: 255 });
-			const ACTIVE_SIX = floatColor({ r: 255, g: 155, b: 0, a: 255 });
+			const ACTIVE = floatColor({ r: 0, g: 155, b: 203, a: 255 });
 
-			this.selection = MenuSelection.None;
+			this.selection = MenuSelection.None(splitMode);
 
 			function setGrey(cr: CairoContext, grey: Grey) {
 				cr.setSourceRGBA(grey.luminance, grey.luminance, grey.luminance, grey.alpha);
@@ -84,22 +92,10 @@ module MenuHandlers {
 			}
 
 			function activeColor(cr: CairoContext, selection: MenuSelection, ring: Ring, location: number) {
-				var active, dark;
-				let state = self.menu.getSplitState();
-				switch (state) {
-					case Menu.SplitMode.FOUR:
-						active = ACTIVE_FOUR;
-						dark = DARK_FOUR;
-						break;
-					case Menu.SplitMode.SIX:
-						active = ACTIVE_SIX;
-						dark = DARK_SIX;
-						break;
-				}
-				if (MenuSelection.eqTo(selection, ring, location, state)) {
-					setColor(cr, active);
+				if (selection.eqPosition(ring, location)) {
+					setColor(cr, ACTIVE);
 				} else {
-					setColor(cr, dark);
+					setColor(cr, DARK);
 				}
 			}
 
@@ -172,16 +168,31 @@ module MenuHandlers {
 				activeColorOuter(cr, selection, Anchor.LEFT);
 				cr.stroke();
 
-				// bottom edge
-				cr.arc(0, 0, OUTER_RADIUS - (EDGE_WIDTH/2), ANGLE_QUARTER - ANGLE_SIXTEENTH, ANGLE_QUARTER + ANGLE_SIXTEENTH);
-				activeColorOuter(cr, selection, Anchor.BOTTOM);
-				cr.stroke();
+				switch (selection.splitMode) {
+					case SplitMode.FOUR:
+						// bottom edge
+						cr.arc(0, 0, OUTER_RADIUS - (EDGE_WIDTH/2), ANGLE_QUARTER - ANGLE_SIXTEENTH, ANGLE_QUARTER + ANGLE_SIXTEENTH);
+						activeColorOuter(cr, selection, Anchor.BOTTOM);
+						cr.stroke();
 
-				// top edge
-				cr.arc(0, 0, OUTER_RADIUS - (EDGE_WIDTH/2), -ANGLE_QUARTER - ANGLE_SIXTEENTH, -ANGLE_QUARTER + ANGLE_SIXTEENTH);
-				activeColorOuter(cr, selection, Anchor.TOP);
-				cr.stroke();
+						// top edge
+						cr.arc(0, 0, OUTER_RADIUS - (EDGE_WIDTH/2), -ANGLE_QUARTER - ANGLE_SIXTEENTH, -ANGLE_QUARTER + ANGLE_SIXTEENTH);
+						activeColorOuter(cr, selection, Anchor.TOP);
+						cr.stroke();
+						break;
 
+					case SplitMode.SIX:
+						// bottom middle
+						cr.arc(0, EDGE_ARC_DISTANCE, CORNER_WIDTH, 0, TAO);
+						activeColorOuter(cr, selection, Anchor.BOTTOM);
+						cr.fill();
+
+						// top middle
+						cr.arc(0, -EDGE_ARC_DISTANCE, CORNER_WIDTH, 0, TAO);
+						activeColorOuter(cr, selection, Anchor.TOP);
+						cr.fill();
+						break;
+				}
 
 				// corner shades:
 				cr.arc(CORNER_DISTANCE, CORNER_DISTANCE, CORNER_WIDTH, 0, TAO);
@@ -204,8 +215,8 @@ module MenuHandlers {
 				cr.setLineWidth(MID_RADIUS - INNER_RADIUS - HALF_GAP_WIDTH);
 				cr.resetClip()
 				cr.rectangle(-HALF.x, -HALF.y, menuSize.x, menuSize.y);
-				switch (self.menu.getSplitState()) {
-					case Menu.SplitMode.FOUR:
+				switch (selection.splitMode) {
+					case SplitMode.FOUR:
 						cr.rectangle(HALF.x, - HALF_GAP_WIDTH, - menuSize.x, GAP_WIDTH);
 						cr.clip();
 
@@ -224,7 +235,7 @@ module MenuHandlers {
 						setGrey(cr, BG);
 						cr.fill();
 						break;
-					case Menu.SplitMode.SIX:
+					case SplitMode.SIX:
 						cr.rotate(ANGLE_TWELFTH);
 						cr.rectangle(HALF.x, - HALF_GAP_WIDTH, - menuSize.x / 2, GAP_WIDTH);
 						cr.rotate(ANGLE_TWELFTH * 4.0);
@@ -234,16 +245,16 @@ module MenuHandlers {
 						cr.clip();
 						cr.rotate(ANGLE_TWELFTH * 3.0);
 
-						cr.arc(0, 0, MID_RADIUS - ((MID_RADIUS - INNER_RADIUS) / 2) - HALF_GAP_WIDTH, 1.0 * ANGLE_TWELFTH, 5.0 * ANGLE_TWELFTH);
+						cr.arc(0, 0, MID_RADIUS - ((MID_RADIUS - INNER_RADIUS) / 2) - HALF_GAP_WIDTH, 1.5 * ANGLE_TWELFTH, 4.5 * ANGLE_TWELFTH);
 						activeColorInner(cr, selection, Anchor.BOTTOM);
 						cr.stroke();
 
 						cr.arc(0, 0, MID_RADIUS - ((MID_RADIUS - INNER_RADIUS) / 2) - HALF_GAP_WIDTH, 5.0 * ANGLE_TWELFTH, 9.0 * ANGLE_TWELFTH);
-						activeColorInner(cr, selection, Anchor.LEFT);
+						activeColorInner(cr, selection, Anchor.TOPLEFT);
 						cr.stroke();
 
 						cr.arc(0, 0, MID_RADIUS - ((MID_RADIUS - INNER_RADIUS) / 2) - HALF_GAP_WIDTH, 9.0 * ANGLE_TWELFTH, 1.0 * ANGLE_TWELFTH);
-						activeColorInner(cr, selection, Anchor.RIGHT);
+						activeColorInner(cr, selection, Anchor.TOPRIGHT);
 						cr.stroke();
 						break;
 				}
@@ -253,34 +264,29 @@ module MenuHandlers {
 			}
 		}
 
-		updateMouseSelection() {
+		updateMenuSelection(splitMode: SplitMode) {
 			const { x, y } = this.currentMouseRelative;
 			const radius = Math.sqrt(Math.pow(x,2) + Math.pow(y,2));
 			const angle = Math.atan2(y, x);
 			// log("radius = " + radius);
 			// log("angle = " + angle);a
-			let state = this.menu.getSplitState();
+			var selection;
 			if (radius <= this.INNER_RADIUS) {
-				this.updateSelection(MenuSelection.None);
+				selection = MenuSelection.None(splitMode);
 			} else if (radius < this.MID_RADIUS) {
-				var index;
-				if (state == Menu.SplitMode.FOUR) {
-					let slice = circularIndex(2, 0)(angle);
-					index = [Anchor.TOP, Anchor.BOTTOM][slice];
-				} else {
-					let slice = circularIndex(3, ANGLE_TWELFTH)(angle);
-					index = [Anchor.LEFT, Anchor.RIGHT, Anchor.BOTTOM][slice];
-				}
-				this.updateSelection(MenuSelection.Inner(index, state));
+				let innerSelection =
+					(splitMode == SplitMode.FOUR) ? innerSelectionFour : innerSelectionSix;
+				selection = MenuSelection.Inner(innerSelection(angle), splitMode);
 			} else {
-				this.updateSelection(MenuSelection.Outer(outerIndex(angle), state));
+				selection = MenuSelection.Outer(outerSelection(angle), splitMode);
 			}
+			this.updateSelection(selection);
 		}
 
-		onMouseMove(mode: MouseMode, eventPoint: Point): ClutterEventResponse {
+		onMouseMove(mode: MouseMode, splitMode: SplitMode, eventPoint: Point): ClutterEventResponse {
 			this.currentMouseRelative = Point.subtract(eventPoint, this.origin);
 			if (mode === MouseMode.MENU) {
-				this.updateMouseSelection();
+				this.updateMenuSelection(splitMode);
 			}
 			this.preview.onMouseMove(mode, eventPoint);
 			return this.Sys.Clutter.EVENT_STOP;
@@ -291,7 +297,7 @@ module MenuHandlers {
 		}
 
 		updateSelection(newSelection: MenuSelection): void {
-			if (!MenuSelection.eq(this.selection, newSelection)) {
+			if (!newSelection.eq(this.selection)) {
 				p("updateSelection(" + JSON.stringify(newSelection) + ")");
 				this.selection = newSelection;
 				this.canvas.invalidate();
@@ -301,37 +307,15 @@ module MenuHandlers {
 
 		applyDirection(direction: Direction) {
 			const currentLocation = this.selection.index as Anchor;
-			const state = this.menu.getSplitState();
+			const selection = this.selection;
 			switch (this.selection.ring) {
 				case Ring.NONE:
-					switch (direction) {
-						case Direction.UP: this.updateSelection(MenuSelection.Outer(Anchor.TOP, state)); break;
-						case Direction.DOWN: this.updateSelection(MenuSelection.Outer(Anchor.BOTTOM, state)); break;
-						case Direction.LEFT: this.updateSelection(MenuSelection.Outer(Anchor.LEFT, state)); break;
-						case Direction.RIGHT: this.updateSelection(MenuSelection.Outer(Anchor.RIGHT, state)); break;
-					}
-				break;
-
 				case Ring.INNER:
-					if (state == Menu.SplitMode.SIX) {
-						switch (direction) {
-							case Direction.LEFT:
-								switch (currentLocation) {
-									case Anchor.RIGHT: this.updateSelection(MenuSelection.Inner(Anchor.BOTTOM, state)); break;
-									case Anchor.BOTTOM: this.updateSelection(MenuSelection.Inner(Anchor.LEFT, state)); break;
-									case Anchor.LEFT: this.updateSelection(MenuSelection.Outer(Anchor.LEFT, state)); break;
-								}
-								break;
-							case Direction.RIGHT:
-								switch (currentLocation) {
-									case Anchor.LEFT: this.updateSelection(MenuSelection.Inner(Anchor.BOTTOM, state)); break;
-									case Anchor.BOTTOM: this.updateSelection(MenuSelection.Inner(Anchor.RIGHT, state)); break;
-									case Anchor.RIGHT: this.updateSelection(MenuSelection.Outer(Anchor.RIGHT, state)); break;
-								}
-								break;
-							case Direction.UP: this.updateSelection(MenuSelection.Outer(Anchor.TOP, state)); break;
-							case Direction.DOWN: this.updateSelection(MenuSelection.Outer(Anchor.BOTTOM, state)); break;
-						}
+					switch (direction) {
+						case Direction.UP: this.updateSelection(selection.Outer(Anchor.TOP)); break;
+						case Direction.DOWN: this.updateSelection(selection.Outer(Anchor.BOTTOM)); break;
+						case Direction.LEFT: this.updateSelection(selection.Outer(Anchor.LEFT)); break;
+						case Direction.RIGHT: this.updateSelection(selection.Outer(Anchor.RIGHT)); break;
 					}
 				break;
 
@@ -339,47 +323,33 @@ module MenuHandlers {
 					switch (direction) {
 						case Direction.UP:
 							switch (currentLocation) {
-								case Anchor.BOTTOMLEFT: this.updateSelection(MenuSelection.Outer(Anchor.LEFT, state)); break;
-								case Anchor.LEFT: this.updateSelection(MenuSelection.Outer(Anchor.TOPLEFT, state)); break;
-								case Anchor.BOTTOMRIGHT: this.updateSelection(MenuSelection.Outer(Anchor.RIGHT, state)); break;
-								case Anchor.RIGHT: this.updateSelection(MenuSelection.Outer(Anchor.TOPRIGHT, state)); break;
-								default: this.updateSelection(MenuSelection.Outer(Anchor.TOP, state)); break;
+								case Anchor.LEFT: this.updateSelection(selection.Outer(Anchor.TOPLEFT)); break;
+								case Anchor.RIGHT: this.updateSelection(selection.Outer(Anchor.TOPRIGHT)); break;
+								default: this.updateSelection(selection.Outer(Anchor.TOP)); break;
 							}
 						break;
 
 						case Direction.DOWN:
 							switch (currentLocation) {
-								case Anchor.TOPLEFT: this.updateSelection(MenuSelection.Outer(Anchor.LEFT, state)); break;
-								case Anchor.LEFT: this.updateSelection(MenuSelection.Outer(Anchor.BOTTOMLEFT, state)); break;
-								case Anchor.TOPRIGHT: this.updateSelection(MenuSelection.Outer(Anchor.RIGHT, state)); break;
-								case Anchor.RIGHT: this.updateSelection(MenuSelection.Outer(Anchor.BOTTOMRIGHT, state)); break;
-								default: this.updateSelection(MenuSelection.Outer(Anchor.BOTTOM, state)); break;
+								case Anchor.LEFT: this.updateSelection(selection.Outer(Anchor.BOTTOMLEFT)); break;
+								case Anchor.RIGHT: this.updateSelection(selection.Outer(Anchor.BOTTOMRIGHT)); break;
+								default: this.updateSelection(selection.Outer(Anchor.BOTTOM)); break;
 							}
 						break;
 
 						case Direction.LEFT:
 							switch (currentLocation) {
-								case Anchor.TOP: this.updateSelection(MenuSelection.Outer(Anchor.TOPLEFT, state)); break;
-								case Anchor.BOTTOM: this.updateSelection(MenuSelection.Outer(Anchor.BOTTOMLEFT, state)); break;
-								case Anchor.RIGHT:
-									if (state == Menu.SplitMode.SIX) {
-										this.updateSelection(MenuSelection.Inner(Anchor.RIGHT, state));
-										break;
-									}
-								default: this.updateSelection(MenuSelection.Outer(Anchor.LEFT, state)); break;
+								case Anchor.TOP: this.updateSelection(selection.Outer(Anchor.TOPLEFT)); break;
+								case Anchor.BOTTOM: this.updateSelection(selection.Outer(Anchor.BOTTOMLEFT)); break;
+								default: this.updateSelection(selection.Outer(Anchor.LEFT)); break;
 							}
 						break;
 
 						case Direction.RIGHT:
 							switch (currentLocation) {
-								case Anchor.TOP: this.updateSelection(MenuSelection.Outer(Anchor.TOPRIGHT, state)); break;
-								case Anchor.BOTTOM: this.updateSelection(MenuSelection.Outer(Anchor.BOTTOMRIGHT, state)); break;
-								case Anchor.LEFT:
-									if (state == Menu.SplitMode.SIX) {
-										this.updateSelection(MenuSelection.Inner(Anchor.LEFT, state));
-										break;
-									}
-								default: this.updateSelection(MenuSelection.Outer(Anchor.RIGHT, state)); break;
+								case Anchor.TOP: this.updateSelection(selection.Outer(Anchor.TOPRIGHT)); break;
+								case Anchor.BOTTOM: this.updateSelection(selection.Outer(Anchor.BOTTOMRIGHT)); break;
+								default: this.updateSelection(selection.Outer(Anchor.RIGHT)); break;
 							}
 						break;
 					}
--- src/menu_selection.ts
+++ src/menu_selection.ts
@@ -1,31 +1,57 @@
-/// <reference path="menu.ts" />
-
 const enum Ring {
 	NONE = 0,
 	INNER,
 	OUTER
 }
 
-interface MenuSelection {
+const enum SplitMode {
+	FOUR,
+	SIX
+}
+
+class MenuSelection {
 	ring: Ring
 	index: number
-	mode: Menu.SplitMode
+	splitMode: SplitMode
+
+	constructor(ring: Ring, index: number, splitMode: SplitMode) {
+		this.ring = ring;
+		this.index = index;
+		this.splitMode = splitMode;
+	}
+
+	eq(b: MenuSelection) {
+		return this.ring == b.ring && this.index == b.index && this.splitMode == b.splitMode;
+	}
+
+	// like eqTo, but ignores splitMode
+	eqPosition(ring: Ring, location: number) {
+		return this.eqTo(ring, location, this.splitMode);
+	}
+
+	eqTo(ring: Ring, location: number, splitMode: SplitMode) {
+		return this.ring == ring && this.index == location && this.splitMode == splitMode;
+	}
+
+	Inner(sel: Anchor): MenuSelection {
+		return MenuSelection.Inner(sel, this.splitMode);
+	}
+
+	Outer(sel: Anchor): MenuSelection {
+		return MenuSelection.Outer(sel, this.splitMode);
+	}
 }
 
 module MenuSelection {
-	export const None: MenuSelection = { ring: Ring.NONE, index: 0, mode: Menu.SplitMode.FOUR }
-
-	export function eq(a: MenuSelection, b: MenuSelection) {
-		return a.ring == b.ring && a.index == b.index && a.mode == b.mode;
-	}
-	export function eqTo(a: MenuSelection, ring: Ring, location: number, mode: Menu.SplitMode) {
-		return a.ring == ring && a.index == location && a.mode == mode;
+	export function None(splitMode: SplitMode): MenuSelection {
+		return new MenuSelection(Ring.NONE, 0, splitMode);
 	}
 
-	export function Inner(sel: Anchor, mode: Menu.SplitMode): MenuSelection {
-		return { ring: Ring.INNER, index: sel, mode: mode };
+	export function Inner(sel: Anchor, splitMode: SplitMode): MenuSelection {
+		return new MenuSelection(Ring.INNER, sel, splitMode);
 	}
-	export function Outer(sel: Anchor, mode: Menu.SplitMode): MenuSelection {
-		return { ring: Ring.OUTER, index: sel, mode: mode };
+
+	export function Outer(sel: Anchor, splitMode: SplitMode): MenuSelection {
+		return new MenuSelection(Ring.OUTER, sel, splitMode);
 	}
 }
