ba3cbf6a55eb6a0ba6e39a09fd76c17e5a4ba358
Tim Cuthbertson <tim@gfxmonk.net>
2024-01-29 20:58:59 +1100

    got nix build working via fetlock

--- nix/default.nix
+++ nix/default.nix
@@ -1,17 +1,24 @@
-{stdenv, nodePackages, gup, glib, enableTests ? false }:
-stdenv.mkDerivation {
-	name = "slinger";
-	src = null; # TODO
-	buildInputs = [ gup glib ] ++ (
-		with nodePackages;
-		[typescript yarn] ++ (if enableTests then [mocha] else [])
-	);
-	buildPhase = ''
-		gup compile
-	'';
-	installPhase = ''
-		dest=$out/share/gnome-shell/extensions
-		mkdir -p $dest
-		cp -r --dereference extension $dest/slinger@gfxmonk.net
-	'';
-}
+{ lib, callPackage, stdenv, nodePackages, yarn-berry, gup, glib, enableTests ? false }:
+let
+	sources = callPackage ./sources.nix {};
+	fetlock = callPackage (sources.fetlock) {};
+	selection = fetlock.yarn.load ./lock.nix {
+		src = sources.local { url = ../.; };
+		pkgOverrides = self: [
+			(self.overrideAttrs {
+				slinger = (o: {
+					buildInputs = o.buildInputs ++ [ gup glib nodePackages.typescript ];
+					buildPhase = o.buildPhase + ''
+						gup compile
+					'';
+					installPhase = ''
+						dest=$out/share/gnome-shell/extensions
+						mkdir -p $dest
+						cp -r --dereference extension $dest/slinger@gfxmonk.net
+					'';
+				});
+			})
+		];
+	};
+in
+	selection.root
